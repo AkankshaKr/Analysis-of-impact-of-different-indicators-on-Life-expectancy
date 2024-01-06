52c8d7a178f5cfe757d008e057d42d689b4775b7
AkankshaKr <56555505+AkankshaKr@users.noreply.github.com>
2024-01-06 16:33:31 +0530

    Init commit

---
+++ IMPACT _FACTORS_ON_LIFE_EXPECTANCY.R
@@ -0,0 +1,113 @@
+install.packages("plm")
+
+library(ggplot2)
+library(readxl)
+library(dplyr)
+library(car)
+library(lmtest)
+library(plm)
+my_data <- read_excel("C:/Users/Ketki/Downloads/Qdapp dataset (1).xlsx")  #Upload the data
+#filtered_data
+my_data<-na.omit(my_data)
+sum(is.na(my_data))
+
+
+# Build the model
+colnames(my_data)[colnames(my_data)=="Life expectancy"] <- "Life_expectancy"
+colnames(my_data)[colnames(my_data)=="Hepatitis B"] <- "Hepatitis_B"
+colnames(my_data)[colnames(my_data)=="HIV/AIDS"] <- "HIVAIDS"
+colnames(my_data)[colnames(my_data)=="Adult Mortality"] <- "Adult_Mortality"
+colnames(my_data)[colnames(my_data)=="percentage expenditure"] <- "percentage_expenditure"
+colnames(my_data)[colnames(my_data)=="Total expenditure"] <- "Total_expenditure"
+colnames(my_data)[colnames(my_data)=="under-five deaths"] <- "under-five_deaths"
+
+
+
+
+
+
+
+
+
+pool<-plm(Life_expectancy ~  percentage_expenditure+Adult_Mortality+Alcohol+Hepatitis_B+Polio+Measles+Diphtheria+BMI+Total_expenditure+HIVAIDS+Schooling,data=my_data,model="pooling")
+summary(pool)
+
+pool<-plm(Life_expectancy ~  percentage_expenditure+Adult_Mortality+Polio+Diphtheria+BMI+HIVAIDS+Schooling,data=my_data,model="pooling")
+summary(pool)
+
+pool<-plm(Life_expectancy ~  percentage_expenditure+Adult_Mortality+Diphtheria+BMI+HIVAIDS+Schooling,data=my_data,model="pooling")
+summary(pool)
+
+
+
+vif(pool)
+str(my_data)
+
+
+
+
+
+
+fixed <- plm(Life_expectancy ~ percentage_expenditure+Adult_Mortality+Diphtheria+BMI+HIVAIDS+Schooling, data=my_data, index=c("Country", "Year"), model="within")  #fixed model
+summary(fixed)
+
+random <- plm(Life_expectancy~percentage_expenditure+Adult_Mortality+Diphtheria+BMI+HIVAIDS+Schooling ,data=my_data, index=c("Country", "Year"), model="random")  #random model
+summary(random)
+phtest(fixed,random) #Hausman test
+
+library(plm)
+# Assuming you have loaded the necessary libraries and defined your my_data dataframe
+
+# Load required libraries
+library(plm)
+library(lmtest)
+
+# Define your random effects model
+random <- plm(Life_expectancy ~ percentage_expenditure + Adult_Mortality + Diphtheria + BMI + HIVAIDS + Schooling,
+              data = my_data, index = c("Country", "Year"), model = "random")
+
+# Summary of the random effects model
+summary(random)
+
+# Breusch-Pagan test for heteroscedasticity
+bp_test_random <- bptest(random)
+print(bp_test_random)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fe_data <- pdata.frame(my_data, index = c("Year"))
+pool<-plm(Life_expectancy ~  percentage_expenditure+Adult_Mortality+Diphtheria+BMI+HIVAIDS+Schooling,data= fe_data, model ="random")
+
+pcdtest(pool, test = c("cd"))
+
+
+
+# Re-run the random effects model with robust standard errors
+random_robust <- plm(Life_expectancy ~ percentage_expenditure + Adult_Mortality + Diphtheria + BMI + HIVAIDS + Schooling,
+                     data = my_data, index = c("Year"), model = "random", effect = "individual", random.method = "swar")
+
+# Summary of the random effects model with robust standard errors
+summary(random_robust)
+# Re-run the random effects model with robust standard errors
+random_robust <- plm(Life_expectancy ~ percentage_expenditure + Adult_Mortality + Diphtheria + BMI + HIVAIDS + Schooling,
+                     data = my_data, index = c("Country"), model = "random", effect = "individual", random.method = "swar")
+
+# Summary of the random effects model with robust standard errors
+summary(random_robust)
+
